b5055f62c345a628281366105f300a734ead5d9a
xiaoiubao1 <cchc09758@gmail.com>
2026-08-10 15:34:05 +0800

    升级随心记到 v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.xiaoiubao.suixinji"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     buildFeatures {
